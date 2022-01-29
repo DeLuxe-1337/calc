a28aa35a51b3516b296e374cc2ed4a88bc18c43d
DeLuxe <74394136+DeLuxe1337001@users.noreply.github.com>
2022-01-29 00:18:50 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var robux_textbox = document.getElementById("robux");
+robux_textbox.oninput = function() {
+    var txt = robux_textbox.value;
+    var amount = parseFloat(txt);
+
+    var final = amount / 1000 * 8.3;
+    final = final.toFixed(2);
+
+    document.getElementById("result").textContent = "Result: " + final
+}
