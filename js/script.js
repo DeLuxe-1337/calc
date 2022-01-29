58a26404a040833f012623fb7742b5c7cb1691e3
DeLuxe <74394136+DeLuxe1337001@users.noreply.github.com>
2022-01-29 00:47:10 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ robux_textbox.oninput = function() {
     var txt = robux_textbox.value;
     var amount = parseFloat(txt);
 
-    var final = amount / 1000 * 8.3;
+    var final = amount / 1000 * 7.05;
     final = final.toFixed(2);
 
     document.getElementById("result").textContent = "Result: " + final
